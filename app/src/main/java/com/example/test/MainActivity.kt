4f0953eef87efdab20aaac90546ac747b939517e
crgab <gabrielcruzds.694@gmail.com>
2025-06-06 21:23:09 -0400

    Banco de dados "Client" adicionado(com erros a serem corrigidos)

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.test.models.Client
+import com.example.test.models.Employee
+import com.example.test.models.Product
+import com.example.test.models.SaleGet
+import com.example.test.services.ApiService
 import com.example.test.ui.theme.TestTheme
-import com.example.test.views.ExibirProdutoPreview
-import com.example.test.views.listaAPIprodutosPreview
+import com.example.test.views.SwipePagerScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,14 +42,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.then(Modifier.fillMaxSize()),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        listaAPIprodutosPreview()
-        ExibirProdutoPreview()
+    var clients by remember { mutableStateOf<List<Client>>(emptyList()) }
+    var products by remember { mutableStateOf<List<Product>>(emptyList()) }
+    var employees by remember { mutableStateOf<List<Employee>>(emptyList()) }
+    var sales by remember { mutableStateOf<List<SaleGet>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        clients = ApiService.getClients()
+        products = ApiService.getProducts()
+        employees = ApiService.getEmployees()
+        sales = ApiService.getSales()
     }
+    SwipePagerScreen(clients, products, employees, sales)
 }
 
 @Preview(showBackground = true)
